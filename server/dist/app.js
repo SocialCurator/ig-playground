459034e2cfbb22bc4ee7856472094ac3a25117db
pauline-ann <paulinebantayan@gmail.com>
2022-05-02 10:07:07 -0700

    import SSR certificate to server

--- server/dist/app.js
+++ server/dist/app.js
@@ -5,10 +5,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
+const fs_1 = __importDefault(require("fs"));
+// import localhost cert
+const key = fs_1.default.readFileSync('./localhost/localhost.decrypted.key');
+const cert = fs_1.default.readFileSync('./localhost/localhost.crt');
+// init app
 const app = (0, express_1.default)();
 const port = 4000;
+// init server
+const https = require('https');
+const server = https.createServer({ key, cert }, app);
 // parse application/json
 app.use(body_parser_1.default.json());
+// routes
+app.get('/', (req, res) => {
+    res.status(200).send('hi I am the server');
+});
 app.post('/api/authorize', (req, res) => {
     console.log(req.body);
     res.json({ yourtoken: `sir!`, originalBody: req.body });
@@ -16,4 +28,7 @@ app.post('/api/authorize', (req, res) => {
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
 });
+// server.listen(port, () => {
+//     return console.log(`Express is listening at https://localhost:${port}`)
+// })
 //# sourceMappingURL=app.js.map
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,26 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import fs from 'fs'
+
+// import localhost cert
+const key = fs.readFileSync('./localhost/localhost.decrypted.key');
+const cert = fs.readFileSync('./localhost/localhost.crt');
+
+// init app
 const app = express()
 const port = 4000
 
+// init server
+const https = require('https')
+const server = https.createServer({ key, cert }, app);
+
 // parse application/json
 app.use(bodyParser.json())
+
+// routes
+app.get('/', (req, res) => {
+    res.status(200).send('hi I am the server')
+})
 
 app.post('/api/authorize', (req, res) => {
     console.log(req.body)
@@ -14,3 +30,7 @@ app.post('/api/authorize', (req, res) => {
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`)
 })
+
+// server.listen(port, () => {
+//     return console.log(`Express is listening at https://localhost:${port}`)
+// })
