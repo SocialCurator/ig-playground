6d756ae208102b7450235df7a8985dac49ceb09e
pauline-ann <paulinebantayan@gmail.com>
2022-05-04 14:55:18 -0700

    make UI similar to figma

--- src/App.tsx
+++ src/App.tsx
@@ -11,26 +11,31 @@ const App = () => {
        initFacebookSdk()
     },[])
 
+    const boxStyle = {
+        backgroundColor: 'lightgrey',
+        padding: '30px',
+        display: 'inline-block'
+    }
+
     return (
         <>
             <Link to="/other">Other</Link>
-            <br /><br />
-            <div>Hello World</div>
             <br />
             <br />
-            {!loggedIn && (
-                <>
-                <h3>Connect your Facebook Account</h3>
-                <button onClick={loginWithFacebook}>Log in with Facebook</button>
-                </>
+            <h3>Connected Accounts</h3>
+            {!loggedIn &&(
+                <div style={boxStyle}>
+                    It looks like you have no connected business account.
+                </div>
             )}
             {loggedIn && user && (
                 <>
-                <h3>Welcome, {user.facebook?.first_name}</h3>
-                <img src={user.facebook?.picture?.data.url} />
-                <button onClick={logoutWithFacebook}>Log out of Facebook</button>
+                <img src={user.facebook?.picture?.data.url} /><span>@{user.facebook?.first_name}</span>
+                <button onClick={logoutWithFacebook}>Disconnect</button>
                 </>
             )}
+                <h3>Add Channels</h3>
+                <button onClick={loginWithFacebook}>Connect your Facebook Account</button>
         </>
     )
 }
