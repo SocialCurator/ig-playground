e8fd21c1fdf598d60c422d60c4344d9322e1673f
pauline-ann <paulinebantayan@gmail.com>
2022-05-10 01:29:33 -0700

    endpoints

--- server/dist/app.js
+++ server/dist/app.js
@@ -16,12 +16,6 @@ const express_1 = __importDefault(require("express"));
 const fs_1 = __importDefault(require("fs"));
 const axios_1 = __importDefault(require("axios"));
 const https_1 = __importDefault(require("https"));
-// TODO: need endpoint to check validity of accessToken
-// TODO: need a function that stores accessToken inside of document for specific user in Mongo, and type of account
-// TODO: separate oAuth process for Ig?
-// ? 3 IG + 3 FB accounts
-// ? need to log into FB to access IG (Business Accounts)
-// ? each log in -> 1 IG + 1 FB?
 // import localhost cert
 const key = fs_1.default.readFileSync('./localhost/localhost.decrypted.key');
 const cert = fs_1.default.readFileSync('./localhost/localhost.crt');
@@ -32,45 +26,114 @@ const server = https_1.default.createServer({ key, cert }, app);
 // parse application/json
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use(express_1.default.json());
-let user = { userId: 'social-curator-id' };
-//------- routes (facebook)
-app.get('/', (req, res) => {
-    res.status(200).send('hi I am the server');
-});
-// store token after login is complete
-app.post('/authorize', (req, res) => {
-    const authResponse = req.body.authResponse;
-    const fbAuth = {
-        accessToken: authResponse.accessToken,
-        userId: authResponse.userID
-    };
-    user.facebook = Object.assign(Object.assign({}, user.facebook), { auth: fbAuth });
-    res.status(201).send(`access token has been stored: ${authResponse.accessToken}`);
-});
-// get token
-app.get('/token', (req, res) => {
-    res.status(200).send(user.facebook.auth.accessToken);
-});
-// get user's facebook profile
-app.get('/getprofile', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const accessToken = user.facebook.auth.accessToken;
-    const url = `https://graph.facebook.com/v13.0/me?fields=id,name,first_name,picture&access_token=${accessToken}`;
+// ------- simulate db
+let user = { userID: 'social-curator-id' };
+//------- functions
+// get user's profile
+const getProfile = (token) => __awaiter(void 0, void 0, void 0, function* () {
+    const url = `https://graph.facebook.com/v13.0/me?fields=id,name,first_name,picture&access_token=${token}`;
     try {
         const { data } = yield (0, axios_1.default)({
             method: 'get',
             url
         });
-        console.log('response from FB req', data);
-        const fbProfile = {
+        console.log('user profile', data);
+        const profile = {
             name: data.name,
             first_name: data.first_name,
             picture: data.picture
         };
-        user.facebook = Object.assign(Object.assign({}, user.facebook), { profile: fbProfile });
-        res.status(200).send(fbProfile);
+        return profile;
     }
     catch (err) {
         console.log(err.message);
+    }
+});
+// get data about pages that user authorized access to
+const getPageInfo = (token) => __awaiter(void 0, void 0, void 0, function* () {
+    const url = `https://graph.facebook.com/v13.0/me/accounts?access_token=${token}`;
+    try {
+        const { data } = yield (0, axios_1.default)({
+            method: 'get',
+            url
+        });
+        // array of objects containing information about each page
+        console.log('response from FB req', data.data);
+        const pageToken = data.data[0].access_token;
+        const pageID = data.data[0].id;
+        return {
+            pageID,
+            pageToken
+        };
+    }
+    catch (err) {
+        console.log(err.message);
+    }
+});
+// post image to user's facebook page
+const postImage = (pageId, imageUrl, pageToken) => __awaiter(void 0, void 0, void 0, function* () {
+    const postUrl = `https://graph.facebook.com/${pageId}/photos?url=${imageUrl}&access_token=${pageToken}`;
+    const { data } = yield (0, axios_1.default)({
+        method: 'post',
+        url: postUrl
+    });
+    console.log('response from FB post image req', data);
+    return data.post_id;
+});
+// update post with caption
+const updatePost = (postId, message, pageToken) => __awaiter(void 0, void 0, void 0, function* () {
+    const updateUrl = `https://graph.facebook.com/${postId}?message=${message}&access_token=${pageToken}`;
+    // update image with caption
+    const { data } = yield (0, axios_1.default)({
+        method: 'post',
+        url: updateUrl
+    });
+    console.log('response from FB update post req', data);
+    return data;
+});
+//------- routes (facebook)
+app.get('/', (req, res) => {
+    res.status(200).send('hi I am the server');
+});
+app.get('/user', (req, res) => {
+    res.status(200).send(user);
+});
+// store token after login is complete
+app.post('/authorize', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const authResponse = req.body.authResponse;
+    const accessToken = authResponse.accessToken;
+    const userID = authResponse.userID;
+    //TODO validate auth token by checking with Facebook if it matches
+    // check if auth token is valid
+    const auth = {
+        accessToken,
+        userID
+    };
+    // get user's profile
+    const profile = yield getProfile(accessToken);
+    // get user's page info
+    const pageInfo = yield getPageInfo(accessToken);
+    // set user data
+    user.facebook = Object.assign(Object.assign({}, user.facebook), { auth, profile, pages: pageInfo });
+    console.log("User's data has been stored in the server.");
+    res.status(201).send(profile);
+}));
+// post to user's facebook page
+app.post('/post', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log('post', req.body);
+    const pageId = user.facebook.pages.pageID;
+    const pageToken = user.facebook.pages.pageToken;
+    const message = req.body.message;
+    const imageUrl = req.body.imageUrl;
+    try {
+        // post image
+        const postId = yield postImage(pageId, imageUrl, pageToken);
+        // update post with caption
+        const data = yield updatePost(postId, message, pageToken);
+        res.send(data);
+    }
+    catch (err) {
+        console.log(err.response);
     }
 }));
 // init server
--- server/src/app.ts
+++ server/src/app.ts
@@ -17,9 +17,19 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
 // ------- simulate db
+
+let user: UserData = {userID: 'social-curator-id'}
+
+// TODO store data for multiple FB + IG accounts
+type UserData = {
+    userID?: string,
+    facebook?: AccountInfo,
+    instagram?: AccountInfo
+}
+
 type AccountInfo = {
     auth?: {
-        userId: string,
+        userID: string,
         accessToken: string,
     },
     profile?: {
@@ -33,45 +43,19 @@ type AccountInfo = {
                 width: number
             }
     }
-    }
+    },
+    pages?: PageInfo
 }
 
-type UserData = {
-    userId?: string,
-    facebook?: AccountInfo,
-    instagram?: AccountInfo
+type PageInfo = {
+    pageID: string,
+    pageToken: string,
 }
 
-let user: UserData = {userId: 'social-curator-id'}
-
-//------- routes (facebook)
-app.get('/', (req, res) => {
-    res.status(200).send('hi I am the server')
-})
-
-// store token after login is complete
-app.post('/authorize', (req, res) => {
-    const authResponse = req.body.authResponse
-
-    const fbAuth = {
-        accessToken: authResponse.accessToken,
-        userId: authResponse.userID
-    }
-
-    user.facebook = {...user.facebook, auth: fbAuth}
-
-    res.status(201).send(`access token has been stored: ${authResponse.accessToken}`)
-})
-
-// get token
-app.get('/token', (req,res)=>{
-    res.status(200).send(user.facebook.auth.accessToken)
-})
-
-// get user's facebook profile
-app.get('/getprofile', async (req, res) => {
-    const accessToken = user.facebook.auth.accessToken
-    const url = `https://graph.facebook.com/v13.0/me?fields=id,name,first_name,picture&access_token=${accessToken}`
+//------- functions
+// get user's profile
+const getProfile = async (token: string) => {
+    const url = `https://graph.facebook.com/v13.0/me?fields=id,name,first_name,picture&access_token=${token}`
 
     try {
         const { data } = await axios({
@@ -79,20 +63,134 @@ app.get('/getprofile', async (req, res) => {
             url
         })
 
-        console.log('response from FB req', data)
+        console.log('user profile', data)
 
-        const fbProfile = {
+        const profile = {
             name: data.name,
             first_name: data.first_name,
             picture: data.picture
         }
 
-        user.facebook = {...user.facebook, profile: fbProfile}
-
-        res.status(200).send(fbProfile)
+        return profile
     }
     catch (err) {
         console.log(err.message)
+    }
+}
+
+// get data about pages that user authorized access to
+const getPageInfo = async (token: string) => {
+    const url = `https://graph.facebook.com/v13.0/me/accounts?access_token=${token}`
+
+    try {
+        const { data } = await axios({
+            method: 'get',
+            url
+        })
+
+        // array of objects containing information about each page
+        console.log('response from FB req', data.data)
+
+        const pageToken = data.data[0].access_token
+        const pageID = data.data[0].id
+
+        return {
+            pageID,
+            pageToken
+        }
+    }
+    catch (err) {
+        console.log(err.message)
+    }
+}
+
+// post image to user's facebook page
+const postImage = async (pageId, imageUrl, pageToken) => {
+    const postUrl = `https://graph.facebook.com/${pageId}/photos?url=${imageUrl}&access_token=${pageToken}`
+
+    const { data } = await axios({
+        method: 'post',
+        url: postUrl
+    })
+   
+    console.log('response from FB post image req', data)
+
+    return data.post_id
+}
+
+// update post with caption
+const updatePost = async (postId, message, pageToken) => {
+    const updateUrl = `https://graph.facebook.com/${postId}?message=${message}&access_token=${pageToken}`
+
+    // update image with caption
+    const { data } = await axios({
+        method: 'post',
+        url: updateUrl
+    })
+
+    console.log('response from FB update post req', data)
+
+    return data
+}
+
+//------- routes (facebook)
+app.get('/', (req, res) => {
+    res.status(200).send('hi I am the server')
+})
+
+app.get('/user', (req, res) => {
+    res.status(200).send(user)
+})
+
+// store token after login is complete
+app.post('/authorize', async (req, res) => {
+    const authResponse = req.body.authResponse
+    const accessToken = authResponse.accessToken
+    const userID = authResponse.userID
+
+    //TODO validate auth token by checking with Facebook if it matches
+    // check if auth token is valid
+
+    const auth = {
+        accessToken,
+        userID
+    }
+
+    // get user's profile
+    const profile = await getProfile(accessToken)
+
+    // get user's page info
+    const pageInfo = await getPageInfo(accessToken)
+
+    // set user data
+    user.facebook = {...user.facebook, auth, profile, pages: pageInfo}
+
+    console.log("User's data has been stored in the server.")
+
+    res.status(201).send(profile)
+})
+
+// post to user's facebook page
+app.post('/post', async (req, res) => {
+
+    console.log('post', req.body)
+
+    const pageId = user.facebook.pages.pageID
+    const pageToken = user.facebook.pages.pageToken
+    const message = req.body.message
+    const imageUrl = req.body.imageUrl
+
+    try {
+        // post image
+        const postId = await postImage(pageId, imageUrl, pageToken)
+
+        // update post with caption
+        const data = await updatePost(postId, message, pageToken)
+
+        res.send(data)
+    }
+    catch (err) {
+        console.log(err.response)
     }
 })
 
