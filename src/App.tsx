13dc957a7283456e610c708fa185f9c8f4bc8d31
pauline-ann <paulinebantayan@gmail.com>
2022-05-10 01:30:59 -0700

    add button for posting + connecting to ig

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,39 @@ import useFacebook from './useFacebook';
 
 const App = () => {
 
-    const { user, loggedIn, actions: { initFacebookSdk, loginWithFacebook, logoutWithFacebook } } = useFacebook()
+    const {
+        user,
+        loggedIn,
+        actions: {
+            initFacebookSdk,
+            loginWithFacebook,
+            logoutWithFacebook,
+            postToFacebook,
+            getUserData }
+        } = useFacebook()
 
     // init Facebook Sdk on page load
     useEffect(()=>{
        initFacebookSdk()
     },[])
 
+    const mainStyle = {
+        fontFamily: 'sans-serif'
+    }
+
     const boxStyle = {
-        backgroundColor: 'lightgrey',
+        backgroundColor: 'lemonchiffon',
         padding: '30px',
-        display: 'inline-block'
+        display: 'inline-block',
+        borderRadius: '20px'
     }
 
     return (
-        <>
+        <div style={mainStyle}>
             <Link to="/other">Other</Link>
             <br />
+            <br />
+            <button onClick={getUserData}>Get User Data</button>
             <br />
             <h3>Connected Accounts</h3>
             {!loggedIn &&(
@@ -32,11 +48,16 @@ const App = () => {
                 <>
                 <img src={user.facebook?.picture?.data.url} /><span>@{user.facebook?.first_name}</span>
                 <button onClick={logoutWithFacebook}>Disconnect</button>
+                <button onClick={postToFacebook}>Post</button>
                 </>
             )}
                 <h3>Add Channels</h3>
                 <button onClick={loginWithFacebook}>Connect your Facebook Account</button>
-        </>
+                <br />
+                <br />
+                // TODO auth process for IG
+                <button disabled>Connect your Instagram Account</button>
+        </div>
     )
 }
 
